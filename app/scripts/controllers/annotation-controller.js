8ed33d7478e177865bacdeb5e6365d404e9e8dfb
tjhubert <tjhubert@live.com>
2015-03-10 20:27:43 -0500

    picasso picture is up

---
+++ app/scripts/controllers/annotation-controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anotareApp.controller:AnnotationCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the anotareApp
+ */
+angular.module('anotareApp')
+  .controller('AnnotationCtrl', function ($scope) {
+    
+  });
